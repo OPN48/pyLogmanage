fb6581b96854da67db6be557b47710151b20bb7a
luke <cuba3@163.com>
2022-02-15 14:29:13 +0800

    规范格式，增加换行

--- logdatatar.py
+++ logdatatar.py
@@ -3,37 +3,37 @@ from logTools.config import *
 from logTools.tools import getInternetIP, getFileSize, sendTheMsgToDingtalk
 
 # 初始化服务器报文 【x.x.x.x】:
-text='【%s】：\n\n' % getInternetIP()
+text = '【%s】：\n\n' % getInternetIP()
 
 # 昨天日期文件名
-today=datetime.date.today()
-yesterdayFileName= 'log-' + str(today - datetime.timedelta(days=1))+'.tar.gz'
+today = datetime.date.today()
+yesterdayFileName = 'log-' + str(today - datetime.timedelta(days=1)) + '.tar.gz'
 # 删除日期文件名
-deleteFileName= 'log-' + str(today - datetime.timedelta(days=deleteDays))+'.tar.gz'
+deleteFileName = 'log-' + str(today - datetime.timedelta(days=deleteDays)) + '.tar.gz'
 
 # 获取文件名及文件大小
 for f in logFileList:
-    fileSize=getFileSize('./'+f)
+    fileSize = getFileSize('./' + f)
     if fileSize >= warnsize:
-        text+='【文件过大】'
-    text+=str(f)+' '+str(fileSize)+'MB \n\n'
+        text += '【文件过大】'
+    text += str(f) + ' ' + str(fileSize) + 'MB \n\n'
 
 # 压缩日志
 try:
     os.system('tar -czvf ./%s %s' % (yesterdayFileName, ' '.join(logFileList)))
-    tarSize=str(getFileSize('./'+yesterdayFileName))
-    text+=yesterdayFileName+' '+tarSize+'MB \n\n'
+    tarSize = str(getFileSize('./' + yesterdayFileName))
+    text += yesterdayFileName + ' ' + tarSize + 'MB \n\n'
 except:
     print('tar error, please check you fileType config')
 
 # 删除所有log,重新创建文件
-os.system('rm -rf ./*%s && touch %s'%(fileType,' '.join(logFileList)))
+os.system('rm -rf ./*%s && touch %s' % (fileType, ' '.join(logFileList)))
 
 # 删除过期日志压缩包
 if os.path.exists('./' + deleteFileName):
     os.system('rm -rf ./' + deleteFileName)
     # 报文增加删除文件
-    text+='删除文件有：'+deleteFileName
+    text += '删除文件有：' + deleteFileName + '\n\n'
 else:
     pass
 
@@ -48,8 +48,8 @@ if isUwsgi:
 df = os.popen('df -lh').read().strip().split('\n')
 for l in df:
     if l[-1] in '/':
-        text += l
+        text += l + '\n\n'
 
 # 发送消息给钉钉
 if isDingtalkMsg:
-    sendTheMsgToDingtalk(text = text)
+    sendTheMsgToDingtalk(text=text)
