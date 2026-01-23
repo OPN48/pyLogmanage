c67db0d20ff82bca7673aa46b7f52c61a1518a12
cuba3 <cuba3@163.com>
2026-01-23 14:15:27 +0800

    修改配置为追加更新模式

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ keyReplace=dict(zip([modelDic[i][0] for i in modelDic],list(safeKeys)))
 
 helpStr='用法: python3 setup.py [-options] [args...](执行初始化配置)\n'
 for key in modelDic:
-    shortKey=modelDic[key][0]
-    msg=modelDic[key][1]
-    default=modelDic[key][2]
+    shortKey = modelDic[key][0]
+    msg = modelDic[key][1]
+    default = modelDic[key][2]
     helpStr += '    -%s -%s    \t%s 默认:%s\n'%(shortKey,key,msg,default)
 
-inputDic=getSysArgv(keyReplace, safeKeys)
+inputDic = getSysArgv(keyReplace, safeKeys)
 
 if not inputDic:
     print(helpStr)
@@ -23,9 +23,12 @@ else:
         f = open(configFileName, 'r')
         for line in f:
             if line.strip():
-                key, value = line.split(' ', 1)
+                key, value = line.strip().split(' ', 1)
+                tempDic[key] = value
+    print(f'此前配置{tempDic}')
+    print(f'输入配置{inputDic}')
     inputDic=tempDic|inputDic
-
+    print(f'合并后配置{inputDic}')
     f = open(configFileName, 'w')
     for key in inputDic:
         if key:
