e4e28fe80e36d45916979afd6111a3ff5317d0d2
cuba3 <cuba3@163.com>
2026-01-27 15:06:53 +0800

    增加编码错误日志try catch上报

--- listener.py
+++ listener.py
@@ -1,24 +1,24 @@
 from logTools.config import *
 from logTools.tools import sendTheMsgToDingtalk, getText
 
-# 初始化服务器报文 【x.x.x.x】:
-text = headerText
-
-if lastLinesNum:
-    text+='The Last '+str(lastLinesNum)+' lines log:\n\n'
-    # print(text)
-else:
-    text+='The all logs:\n\n'
-defaultText = text
-# 每秒监听
-print(text,logFileList)
-text=getText(text,logFileList,step=1)
-# 无数据提升至每分钟监听
-if defaultText == text:
-    text = getText(text, logFileList, step=60)
-    
-if defaultText == text:
-    text += 'Had not APIs more than ' + str(oneSecondMaxlog) + ' requests per second or minute'
-# print(text)
+try:
+    # 初始化服务器报文 【x.x.x.x】:
+    text = headerText
+    if lastLinesNum:
+        text += 'The Last ' + str(lastLinesNum) + ' lines log:\n\n'
+        # print(text)
+    else:
+        text += 'The all logs:\n\n'
+    defaultText = text
+    # 每秒监听
+    # print(text,logFileList)
+    text = getText(text, logFileList, step=1)
+    # 无数据提升至每分钟监听
+    if defaultText == text:
+        text = getText(text, logFileList, step=60)
+    if defaultText == text:
+        text += 'Had not APIs more than ' + str(oneSecondMaxlog) + ' requests per second or minute'
+except Exception as e:
+    text = e
 if isDingtalkMsg:
     sendTheMsgToDingtalk(text=text)
